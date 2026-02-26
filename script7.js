d49fe4db7d2d63093e4f4d609f7b9585243ef653
jawwad07 <134841966+jawwad07@users.noreply.github.com>
2026-02-26 07:28:58 -0800

    Task-07
    Creating two JS files one exports the functions and other imports its objects and uses it to display or calculate values

---
+++ script7.js
@@ -0,0 +1,3 @@
+import { jawwadProfile, greetUser } from "./utils7.js";
+console.log(jawwadProfile);
+greetUser(jawwadProfile.name);
---
+++ utils7.js
@@ -0,0 +1,9 @@
+export const jawwadProfile={
+    name:"Jawwad",
+    age:25,
+    profession:"Web Developer"
+};
+
+export const greetUser=(name)=>{
+    console.log("Hello",name,"Welcome to BSIT-A");
+}
