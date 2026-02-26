e2ce719e47b7bf5fdeea86c207f48b7969aef62b
jawwad07 <134841966+jawwad07@users.noreply.github.com>
2026-02-26 08:26:15 +0500

    Task-01

---
+++ script.js
@@ -0,0 +1,8 @@
+const name="Jawwad";
+let jAge=19;
+console.log(name);
+console.log(jAge);
+jAge=20;
+console.log(jAge);
+ name="Ahmad"; //This will provide error because we cannot reassign const variables
+
