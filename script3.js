0a641f70b0f9999bc7c385b716ccc866b656f407
jawwad07 <134841966+jawwad07@users.noreply.github.com>
2026-02-26 09:22:08 +0500

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,17 @@
+const jawwadProfile={
+    name:"Jawwad Ahmad",
+    id:2502128,
+    grade:'A'
+};
+
+
+const {name,grade}=jawwadProfile;
+console.log(name);
+console.log(grade);
+
+
+const jawwadFoods=["Biryani", "Pakoray"];
+const allFoods=[...jawwadFoods, "Pulao"];
+
+console.log(jawwadFoods);
+console.log(allFoods);
